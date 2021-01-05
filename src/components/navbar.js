c74b63e55aa8f130a69c02af335df89e350cea62
Iden Han <idenhan129@gmail.com>
2021-01-05 23:55:44 +0900

    Implement Navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,51 @@
+import logo from "../assets/logo.svg";
+
+/* eslint-disable jsx-a11y/anchor-is-valid */
+const Navbar = () => {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img src={logo} className="Nav-logo" alt="logo" />
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0 text-end">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">
+                보험찾기
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                청구안내
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                이벤트
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                로그인
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
