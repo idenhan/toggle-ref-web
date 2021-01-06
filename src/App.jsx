68fc36b568ffe3100a4ac77bca95a88e416c756b
Toggle <idenhan129@gmail.com>
2021-01-06 14:13:03 +0900

    Change input type

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import Navbar from "./components/navbar";
 import Register from "./components/registerForm";
 
--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -1,4 +1,5 @@
 import logo from "../assets/logo-insurance.svg";
+import view from "../assets/views-categories-label.svg";
 import "./registerForm.css";
 
 /* eslint-disable jsx-a11y/anchor-is-valid */
@@ -51,55 +52,55 @@ const Register = () => {
                   주택종류
                 </label>
                 <div class="input-group mb-3">
-                  {/* <input
+                  <input
                     type="text"
                     class="form-control"
                     placeholder="주택종류를 선택해 주세요."
                     aria-label="주택종류를 선택해 주세요."
                     aria-describedby="basic-addon1"
-                  /> */}
-                  <select class="form-select" id="inputGroupSelect01">
+                  />
+                  {/* <select class="form-select" id="inputGroupSelect01">
                     <option selected>주택종류를 선택해 주세요.</option>
                     <option value="1">빌라</option>
                     <option value="2">아파트</option>
                     <option value="3">단독주택</option>
-                  </select>
+                  </select> */}
                 </div>
                 <label for="basic-url" class="form-label">
                   화재보험(건물) 가입금액
                 </label>
                 <div class="input-group mb-3">
-                  {/* <input
+                  <input
                     type="text"
                     class="form-control"
                     placeholder="화재보험(건물)"
                     aria-label="화재보험(건물)"
                     aria-describedby="basic-addon1"
-                  /> */}
-                  <select class="form-select" id="inputGroupSelect01">
+                  />
+                  {/* <select class="form-select" id="inputGroupSelect01">
                     <option selected>화재보험(건물)</option>
                     <option value="1">서울에서</option>
                     <option value="2">집을</option>
                     <option value="3">어케사냐</option>
-                  </select>
+                  </select> */}
                 </div>
                 <label for="basic-url" class="form-label">
                   화재보험(가재도구) 가입금액
                 </label>
                 <div class="input-group mb-3">
-                  {/* <input
+                  <input
                     type="text"
                     class="form-control"
                     placeholder="화재보험(가재도구)"
                     aria-label="화재보험(가재도구)"
                     aria-describedby="basic-addon1"
-                  /> */}
-                  <select class="form-select" id="inputGroupSelect01">
+                  />
+                  {/* <select class="form-select" id="inputGroupSelect01">
                     <option selected>화재보험(가재도구)</option>
                     <option value="1">서울에서</option>
                     <option value="2">집을</option>
                     <option value="3">어케사냐</option>
-                  </select>
+                  </select> */}
                 </div>
                 <label for="basic-url" class="form-label">
                   주소
@@ -117,20 +118,20 @@ const Register = () => {
                   면적정보
                 </label>
                 <div class="input-group mb-3">
-                  {/* <input
+                  <input
                     type="text"
                     class="form-control"
                     placeholder="면적을 알려주세요."
                     aria-label="면적을 알려주세요."
                     aria-describedby="basic-addon1"
-                  /> */}
-                  <select class="form-select" id="inputGroupSelect01">
+                  />
+                  {/* <select class="form-select" id="inputGroupSelect01">
                     <option selected>면적을 알려주세요.</option>
                     <option value="1">20평 이하</option>
                     <option value="2">40평 이하</option>
                     <option value="3">80평 이하</option>
                     <option value="3">100평 이상</option>
-                  </select>
+                  </select> */}
                 </div>
                 <div id="ins-form-category">
                   <span id="bar">|</span>
@@ -169,6 +170,28 @@ const Register = () => {
                 </div>
               </div>
               <div className="col">a</div>
+            </div>
+          </div>
+          <div id="ins-form-category">
+            <span id="bar">|</span>
+            <span id="category">보장내용</span>
+          </div>
+          <div>
+            <div className="row">
+              <div className="col">
+                <div className="mb-2">화재/폭발/파열(건물)</div>
+                <div className="mb-2">화재/폭발/파열(가재)</div>
+                <div className="mb-2">배상책임</div>
+                <div className="mb-2">
+                  <img src={view} className="view-label" alt="view" />
+                </div>
+              </div>
+              <div className="col"></div>
+              <div className="col">
+                <div className="mb-2">보험료 선택</div>
+                <div className="mb-2">보험료 선택</div>
+                <div className="mb-2">1억원</div>
+              </div>
             </div>
           </div>
         </div>
