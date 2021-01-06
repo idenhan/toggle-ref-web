7c7ee67ac2e651d79bc2391eec4c4bc34e6f5d0a
Toggle <idenhan129@gmail.com>
2021-01-06 12:13:17 +0900

    Add insurance logo, name, and register form box

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -1,15 +1,21 @@
 import logo from "../assets/logo-insurance.svg";
+import "./registerForm.css";
 
 /* eslint-disable jsx-a11y/anchor-is-valid */
 const Register = () => {
   return (
     <div className="container-fluid">
-      <div className="row">
+      <div className="row" id="wrapper">
         <div className="col"></div>
         <div className="col-5">
           <div>
-            <img src={logo} className="reg-logo" alt="logo" />
+            <img src={logo} className="ins-logo" alt="logo" />
           </div>
+          <div>
+            <span id="ins-name">CM01700006</span>
+            <span id="ins-name-extra">| 표준플랜</span>
+          </div>
+          <div id="ins-form">dsfasdf</div>
         </div>
         <div className="col"></div>
       </div>
